d1db0bb477932d90e04435c03e333e4558c5d2e7
Dmitry Bessonov <dmitrybessonov@icloud.com>
2021-03-15 17:10:16 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
       currentVideo: null,
       isPlayerOpen: false,
       videos: [],
-      youTubeData: null
+      youTubeData: null,
     }
   },
   components: {
@@ -43,7 +43,7 @@ export default {
     async getDataFromYT() {
       const YOUTUBE_PLAYLIST_ITEMS_API = 'https://www.googleapis.com/youtube/v3/playlistItems'
       const YOUTUBE_PLAYLIST = 'PLBjThViczbwqRPoX01nNR3vppD1nCy_7c'
-      const YOUTUBE_API_KEY = JSON.stringify(process.env.YOUTUBE_API_KEY)
+      const YOUTUBE_API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY
 
       const firstPart = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?key=${YOUTUBE_API_KEY}&playlistId=${YOUTUBE_PLAYLIST}&part=snippet&maxResults=50&pageToken=CDIQAQ`)
       const secondPart = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?key=${YOUTUBE_API_KEY}&playlistId=${YOUTUBE_PLAYLIST}&part=snippet&maxResults=38&pageToken=CDIQAA`)
